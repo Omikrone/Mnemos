d24a7ada8c3c495cf4891de66be8743ee01b5c4e
Roman NITZSCHE <roman.nitzsche67@gmail.com>
2025-07-27 12:32:43 +0200

    Adding multiple masks dropout

--- model/dropout.py
+++ model/dropout.py
@@ -4,21 +4,26 @@ import numpy as np
 class Dropout:
     def __init__(self, rate: float):
         self.rate = rate
-        self.mask = None
+        self.masks = []
+
 
     def forward(self, x: np.ndarray, train: bool = True) -> np.ndarray:
         if train and self.rate > 0.0:
-            self.mask = (np.random.rand(*x.shape) > self.rate).astype(np.float32)
-            return x * self.mask / (1.0 - self.rate)
+            mask = (np.random.rand(*x.shape) > self.rate).astype(np.float32)
+            self.masks.append(mask)
+            return x * mask / (1.0 - self.rate)
         else:
             self.mask = np.ones_like(x)
-            return x  # pas de dropout en inference
+            return x
+
 
     def backward(self, grad_output: np.ndarray) -> np.ndarray:
-        return grad_output * self.mask / (1.0 - self.rate)
+        mask = self.masks.pop()
+        return grad_output * mask / (1.0 - self.rate)
     
+
     def zero_grad(self):
         """ Reset the dropout mask. """
-        
-        self.mask = None
+
+        self.masks = []
         
