9dec151c9278478f5ec9f70669f2ed118621fe34
Roman Nitzsche <roman.nitzsche67@gmail.com>
2026-01-30 23:28:42 +0100

    feat(deploy): add docker compose file to launch web and api

--- backend/app/main.py
+++ backend/app/main.py
@@ -8,7 +8,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
-        "http://localhost:8000",
+        "http://localhost:49182",
+        "http://mnemos.duckdns.org:49182",
         "http://localhost:80",
         "http://127.0.0.1:80",
         "http://localhost",
