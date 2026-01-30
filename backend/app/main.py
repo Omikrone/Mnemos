76f23932ffb8b83d761a7c2b27dab1b1c9729a76
Roman Nitzsche <roman.nitzsche67@gmail.com>
2026-01-30 22:56:44 +0100

    feat(deploy): add deploy for api

--- backend/app/main.py
+++ backend/app/main.py
@@ -8,7 +8,10 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
-        "http://localhost:5173",
+        "http://localhost:8000",
+        "http://localhost:80",
+        "http://127.0.0.1:80",
+        "http://localhost",
     ],
     allow_credentials=True,
     allow_methods=["*"],
